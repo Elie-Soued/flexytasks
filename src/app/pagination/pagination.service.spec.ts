620e2611d3ecb6a9e57c4d995df36ab02139055c
Elie Soueid <eliegebransoued@gmail.com>
2025-05-30 10:41:30 +0200

    adding pagination tests

--- src/app/pagination/pagination.service.spec.ts
+++ src/app/pagination/pagination.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { PaginationService } from './pagination.service';
+import { HttpParams } from '@angular/common/http';
 
 describe('PaginationService', () => {
   let service: PaginationService;
@@ -8,32 +9,27 @@ describe('PaginationService', () => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(PaginationService);
   });
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+
+  it('should initialize with default values', () => {
+    expect(service.offset()).toBe(0);
+    expect(service.limit()).toBe(5);
+    expect(service.totalCount()).toBe(0);
   });
 
-  // it('The value emitted by emitOffset is correctly caught by the subscription on offser$', () => {
-  //   service.emitOffset(3);
-  //   service.offset$.subscribe((data) => {
-  //     expect(data).toEqual(3);
-  //   });
-  // });
-  // it('The value emitted by emitTotalCount is correctly caught by the subscription on totalCount$', () => {
-  //   service.emitTotalCount(3);
-  //   service.totalCount$.subscribe((data) => {
-  //     expect(data).toEqual(3);
-  //   });
-  // });
-  // it('An event is triggered by emitNextPage and this event can be subscripted to using the nextPage$ Observable  ', () => {
-  //   service.emitNextPage();
-  //   service.nextPage$.subscribe((data) => {
-  //     expect(data).toBeUndefined;
-  //   });
-  // });
-  // it('An event is triggered by emitPreviousPage and this event can be subscripted to using the previousPage$ Observable', () => {
-  //   service.emitPreviousPage();
-  //   service.previousPage$.subscribe((data) => {
-  //     expect(data).toBeUndefined;
-  //   });
-  // });
+  it('updateOffset is correctly executed', () => {
+    service.updateOffset(10);
+    expect(service.offset()).toBe(10);
+  });
+
+  it('updateTotalCount is correctly executed', () => {
+    service.updateTotalCount(20);
+    expect(service.totalCount()).toBe(20);
+  });
+
+  it('addPaginationParams is correctly executed', () => {
+    service.updateOffset(20);
+    const params: HttpParams = service.addPaginationParams();
+    expect(params.get('offset')).toBe('20');
+    expect(params.get('limit')).toBe('5');
+  });
 });
