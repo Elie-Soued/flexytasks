8f88b66a82ca61db789a2e0e1492dbc0ac6fa46a
Elie Soueid <eliegebransoued@gmail.com>
2025-06-12 08:34:17 +0200

    move the effect in the constructor

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -1,30 +1,22 @@
-import { Component, effect, Injector, OnInit } from '@angular/core';
+import { Component, effect } from '@angular/core';
 import { PaginationService } from './pagination.service';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent {
   limit = 0;
   offset = 0;
   totalCount = 0;
 
-  constructor(
-    private paginationService: PaginationService,
-    private injector: Injector
-  ) {}
-
-  ngOnInit(): void {
-    effect(
-      () => {
-        // We track offset, totalCount and limit to enable/disable the previous and next buttons
-        this.offset = this.paginationService.offset();
-        this.totalCount = this.paginationService.totalCount();
-        this.limit = this.paginationService.limit();
-      },
-      { injector: this.injector }
-    );
+  constructor(private paginationService: PaginationService) {
+    effect(() => {
+      // We track offset, totalCount and limit to enable/disable the previous and next buttons
+      this.offset = this.paginationService.offset();
+      this.totalCount = this.paginationService.totalCount();
+      this.limit = this.paginationService.limit();
+    });
   }
 
   nextPage(): void {
